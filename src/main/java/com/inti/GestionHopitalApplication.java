ad64dccdb76d23bc850370bfb3496f92e052a15f
root <root@localhost.localdomain>
2022-01-20 05:45:09 -0500

    starter security corrige

--- src/main/java/com/inti/GestionHopitalApplication.java
+++ src/main/java/com/inti/GestionHopitalApplication.java
@@ -2,12 +2,22 @@ package com.inti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = 
+{@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfiguration.class)})
 public class GestionHopitalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionHopitalApplication.class, args);
 	}
-
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/inti/ServletInitializer.java
+++ src/main/java/com/inti/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GestionUtilisateurApplication.class);
+		return application.sources(GestionHopitalApplication.class);
 	}
 
 }
