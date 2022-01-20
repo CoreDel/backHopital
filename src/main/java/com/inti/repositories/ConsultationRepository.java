de52bff155d3bf7338cf45db92f5bb34b077f469
nobelios <nobelios@gmail.com>
2022-01-20 14:21:17 +0100

    Remplissage de la casse ConsultationController

--- src/main/java/com/inti/repositories/ConsultationRepository.java
+++ src/main/java/com/inti/repositories/ConsultationRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.inti.entities.Consultation;
+import com.inti.entities.DossierMedical;
 
 @Repository
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
-
+	Consultation findByIdConsultation(Long idConsultation);
 }
